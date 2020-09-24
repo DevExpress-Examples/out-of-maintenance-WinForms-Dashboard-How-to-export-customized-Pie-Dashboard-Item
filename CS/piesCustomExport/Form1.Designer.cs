773b755176a6e2362254f5ee74ed0e719311f905
Margarita Zakhodyaeva (DevExpress) <zakhodyaeva@corp.devexpress.com>
2020-09-24 20:51:38 +0300

    add CS

---
+++ CS/piesCustomExport/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace piesCustomExport
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dashboardDesigner1 = new DevExpress.DashboardWin.DashboardDesigner();
+            ((System.ComponentModel.ISupportInitialize)(this.dashboardDesigner1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dashboardDesigner1
+            // 
+            this.dashboardDesigner1.AllowInspectAggregatedData = true;
+            this.dashboardDesigner1.AllowInspectRawData = true;
+            this.dashboardDesigner1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dashboardDesigner1.Location = new System.Drawing.Point(0, 0);
+            this.dashboardDesigner1.Name = "dashboardDesigner1";
+            this.dashboardDesigner1.Size = new System.Drawing.Size(1096, 473);
+            this.dashboardDesigner1.TabIndex = 0;
+            this.dashboardDesigner1.DashboardItemControlUpdated += new DevExpress.DashboardWin.DashboardItemControlUpdatedEventHandler(this.dashboardDesigner1_DashboardItemControlUpdated);
+            this.dashboardDesigner1.CustomExport += new DevExpress.DashboardCommon.CustomExportEventHandler(this.dashboardDesigner1_CustomExport);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1096, 473);
+            this.Controls.Add(this.dashboardDesigner1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dashboardDesigner1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.DashboardWin.DashboardDesigner dashboardDesigner1;
+    }
+}
---
+++ CS/piesCustomExport/Form1.cs
@@ -0,0 +1,58 @@
+﻿using DevExpress.DashboardCommon;
+using DevExpress.DashboardWin;
+using DevExpress.XtraCharts;
+using DevExpress.XtraReports.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace piesCustomExport
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dashboardDesigner1.CreateRibbon();
+            dashboardDesigner1.LoadDashboard("nwind.xml");
+        }
+
+        private void dashboardDesigner1_DashboardItemControlUpdated(object sender, DevExpress.DashboardWin.DashboardItemControlEventArgs e)
+        {
+            if (e.ChartControl != null)
+            {
+                PieDashboardItem pieItem = dashboardDesigner1.Dashboard.Items[e.DashboardItemName] as PieDashboardItem;
+                if (pieItem == null) return;
+                foreach (Series series in e.ChartControl.Series)
+                {
+                    (series.View as PieSeriesView).TotalLabel.Visible = true;
+                }
+            }
+        }
+
+        private void dashboardDesigner1_CustomExport(object sender, CustomExportEventArgs e)
+        {
+            foreach (var printControl in e.GetPrintableControls())
+            {
+                if (printControl.Value is XRChart)
+                {
+                    var pieItemName = printControl.Key;
+                    IDashboardControl dashboardControl = (IDashboardControl)sender;
+                    PieDashboardItem pieDashboardItem = dashboardControl.Dashboard.Items[pieItemName] as PieDashboardItem;
+                    if (pieDashboardItem == null) return;
+                    XRChart pieChart = printControl.Value as XRChart;
+                    foreach (Series series in pieChart.Series)
+                    {
+                        (series.View as PieSeriesView).TotalLabel.Visible = true;
+                    }
+                }
+            }
+        }
+    }
+}
